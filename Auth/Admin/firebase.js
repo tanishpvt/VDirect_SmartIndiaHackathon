80358f5d57c539e04b3512c5715afdeabd4cbae2
tanishpvt <tanish0911@gmail.com>
2022-03-11 00:12:29 +0530

    Auth
    Working on auth for admin and user

---
+++ Auth/Admin/firebase.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDn0YLSUKggpOhhhnI5UBREb0QuEo1nyJs",
+    authDomain: "fluffy-2ea00.firebaseapp.com",
+    databaseURL: "https://fluffy-2ea00.firebaseio.com",
+    projectId: "fluffy-2ea00",
+    storageBucket: "fluffy-2ea00.appspot.com",
+    messagingSenderId: "72613846505",
+    appId: "1:72613846505:web:4de27416d540c887f13f78",
+    measurementId: "G-WX371X996X"
+  };
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
